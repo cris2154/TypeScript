9e034c0537ece603e210bf203ccab860b4215773
cris <crishuamani023@gmail.com>
2026-05-07 00:48:04 -0500

    seccion 2 ts asta acercion de tipos

---
+++ TypescriptBasic/2.ts
@@ -0,0 +1,129 @@
+ 
+// //norrowing o estrechamiento de tipo 
+// //basicamente typescript deduce que tipo de dato es al momento de crear una llamada de funcion 
+
+
+// function ProcesarDato(valor : string|number):void{
+//     if(typeof valor === "string"){
+//         console.log(valor.length)
+//     }
+// }
+
+// ProcesarDato("pepe")
+// function saludar(nombre: string | null) {
+//   if (nombre) {
+//     // Si 'nombre' existe (no es null ni ""), TS lo trata como string
+//     console.log(`Hola ${nombre.toUpperCase()}`);
+//   }
+// }
+// saludar("pepe")
+
+// //en objetos 
+// type Pajaro = { volar: () => void };
+// type Pez = { nadar: () => void };
+
+// function mover(animal: Pajaro | Pez) {
+//   if ("volar" in animal ) {
+//     // in si la propiedad volar esta en uno de los typos espesicifacos en animal
+//     // Aquí TS sabe que es un Pajaro
+//     animal.volar();
+//   } else {
+//     // Aquí sabe que es un Pez
+//     animal.nadar();
+//   }
+// }
+
+// //ejercisio
+// function obtenerLongitud(dato: string | number) {
+//     // 1. ¿Cómo comprobamos si 'dato' es un arreglo?
+//     if (typeof dato === "number") {
+//         // Aquí TS sabe que es string[] 
+//         console.log("tu numero es :",dato);
+//     } else {
+//         // 2. ¿Qué sabe TS que es 'dato' aquí dentro?
+//         console.log("Cantidad de letras:", dato.length);
+//     }
+// }
+
+// let cosas = ["pepe","carlos"]
+// let cosa = "peluquero"
+// let numeroRandon = 88
+
+// obtenerLongitud(numeroRandon)
+
+// //enums o enumeradores 
+// //genera una lista finita de datos enumerandorlos desde 0 o definiendo su respuesta con =
+// enum EstadoTransaccion {
+//     Pendiente="Pendiente", // 0
+//     Pagado="Pagado",    // 1
+//     Error = "errpr"     // 2
+// }
+
+// let miPago = EstadoTransaccion.Pagado;
+
+// if (miPago === EstadoTransaccion.Pagado) {
+//     console.log(EstadoTransaccion.Pagado);//"1"//ahora es "pagado" al definir su valor
+// }
+
+// // Con INTERFACE
+// interface Personaje {
+//     nombre: string;
+// }
+
+// interface Personaje { // Se fusiona con la anterior
+//     vida: number;
+// }
+
+// // Con TYPE
+// type Guerrero = {
+//     ataque: number;
+// } & Personaje; // Usamos intersección para "heredar"
+
+// let carlos : Personaje = {
+//     nombre : "pepe",
+//     vida : 100
+// }
+
+// let pedro : Guerrero ={
+//     ataque : 55,
+//     nombre : "pedro",
+//     vida : 100
+// }
+
+// //ejercisio 
+
+ const enum Clase {
+     Mago = "MAGO",
+     Guerrero = "GUERRERO",
+     Arquero = "ARQUERO"
+ }
+
+ interface Heroe {   
+     nombre: string;
+     tipo: Clase;
+}
+
+// function activarHabilidad(personaje : Heroe) {
+//     if(personaje.tipo === "ARQUERO"){
+//         console.log("tu personaje es ",personaje.tipo)
+//     }else(console.log("tu personaje no es un arquero"))
+// }
+
+// let Martin : Heroe ={
+//     nombre: "martin",
+//     tipo : Clase.Arquero
+// }
+
+// activarHabilidad(Martin)
+
+
+// Imaginemos que esto viene de una base de datos externa
+const respuestaApi: any = { nombre: "Legolas", tipo: "ARQUERO" };
+
+// Le decimos a TS: "Confía en mí, esto es un Heroe"
+const nuevoHeroe = respuestaApi as Heroe; 
+
+console.log(nuevoHeroe.tipo); // Ahora TS nos da autocompletado
+
+let datoDesconocido : any = 100;
+let puntos = datoDesconocido as number
